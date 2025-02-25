37dbf61ab1962d71f1b57fa324c13e65bc63c86d
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-02-25 09:57:25 +0100

    changes

---
+++ models/carModels.js
@@ -0,0 +1,29 @@
+import dbConfig from '../Config/dbConfig.js';
+import { DataTypes, Model } from 'sequelize';
+
+export class carModel extends Model {}
+carModel.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },
+    
+      brand: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    
+      year: {
+        type: DataTypes.DATE,
+        allowNull: false
+      }
+    }, {
+        sequelize: dbConfig,
+        modelName: 'car', // Modellens navn
+        underscored: true, // True: car_brands || False: carBrands
+        freezeTableName: true, // True: car || False: cars
+        createdAt: true, // Tilføjer createdAt felt
+        updatedAt: true, // Tilføjer updatedAt felt
+      });
